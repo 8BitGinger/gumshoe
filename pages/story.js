4141ba1dbd3efd8cf802457deaf8a03abe7d968c
8BitGinger <ryan.fann>
2024-03-05 04:00:28 -0600

    updated corrected pictures for Police Station section, added some animation for Casino section and corrected margins

--- pages/story.js
+++ pages/story.js
@@ -1,5 +1,6 @@
 const icon = document.getElementById('iconST');
 const ghost = document.getElementById('ghost');
+const roulette = document.getElementById('roulette');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 0) {
@@ -11,5 +12,5 @@ window.addEventListener('scroll', () => {
 
 function spinIcon() {
   icon.style.transform = `rotate(${window.scrollY}deg)`;
-  // ghost.style.transform = `rotate(${window.scrollY}deg)`;
+  roulette.style.transform = `rotate(${window.scrollY}deg)`;
 }
